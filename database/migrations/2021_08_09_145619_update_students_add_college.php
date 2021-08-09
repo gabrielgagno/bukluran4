14e73e9ed1924bf660a48c14a7657b66894f9b81
Gabriel John Gagno <gpgagno@up.edu.ph>
2021-08-09 23:45:02 +0800

    Created Student and College models and migrations - Added initial migrations, factories, and seeders - Added models and created relations between the models
    References #9

---
+++ database/migrations/2021_08_09_145619_update_students_add_college.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStudentsAddCollege extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->integer('college_id')->references('id')->on('colleges')->after('contact_num');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('college_id');
+        });
+    }
+}
